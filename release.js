d8292fa8a2b1f1393d83a86a96eeedce9bb82e9b
Oniel <51058739+0niel@users.noreply.github.com>
2021-06-13 22:14:35 +0300

    Fix chat html

--- release.js
+++ release.js
@@ -22,14 +22,12 @@
                     <label for="chat-button" class="chat-button" onclick></label>
                     <section>
                         <div id="chat-messages" style="max-height: 700px; overflow-y: scroll; padding-right: 10px;">
-     
                         </div>
-
                         <div>
                             <textarea id="chat-input" placeholder="Написать сообщение"></textarea>
                             <svg width="33" height="32" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg" id="send-chat-message-icon">
                                 <path d="M28.2758 4.29001C28.1423 4.15569 27.9738 4.06268 27.7898 4.02194C27.6059 3.9812 27.4143 3.99441 27.2376 4.06001L5.48526 12.06C5.29766 12.132 5.13615 12.26 5.02218 12.427C4.90821 12.594 4.84717 12.7921 4.84717 12.995C4.84717 13.1979 4.90821 13.3961 5.02218 13.5631C5.13615 13.7301 5.29766 13.858 5.48526 13.93L13.9786 17.36L20.2472 11L21.6413 12.41L15.343 18.78L18.7443 27.37C18.8176 27.5561 18.9444 27.7156 19.1083 27.8279C19.2723 27.9403 19.4658 28.0002 19.6638 28C19.8636 27.9959 20.0575 27.9306 20.2199 27.8128C20.3823 27.6949 20.5056 27.5301 20.5735 27.34L28.4834 5.34001C28.5508 5.16309 28.567 4.97044 28.5303 4.78454C28.4935 4.59863 28.4052 4.42712 28.2758 4.29001Z" fill="#377DFF"/>
-                            </svg>  
+                            </svg>
                         </div>
                     </section>
                 </nav>
@@ -39,11 +37,9 @@
                 .chat-nav {
                     width: 485px;
                     height: 800px;
-        
                     background: #EFEFEF;
                     box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
                     border-radius: 20px;
-        
                     position: fixed;
                     padding: 0;
                     top: 16.7%;
@@ -58,12 +54,10 @@
                     transition: 0.34s;
                     z-index: 9998;
                 }
-        
                 .chat-nav > section {
                     padding: 15px;
-                    color: #8D8D8D 
+                    color: #8D8D8D
                 }
-        
                 .chat-button {
                     position: absolute;
                     right: 99.8%;
@@ -85,96 +79,79 @@
                     font: normal 18px/1 'FontAwesome';
                     text-decoration: inherit;
                 }
-        
                 .chat-button:hover{
                     cursor: pointer;
                     color:#1f1c1c;
                 }
-        
                 [id='chat-button'] {
                     position: absolute;
                     right:0;
                     display:none;
                 }
-        
                 [id='chat-button']:checked ~ .chat-nav {
                     -webkit-transform: translateX(0);
                     -moz-transform: translateX(0);
                     transform: translateX(0);
                 }
-        
                 [id='chat-button']:checked ~ .chat-nav > .chat-button:after {
                     content:'>';
                     font: normal 18px/1 'FontAwesome';
                 }
-                body { 
+                body {
                     -webkit-animation: bugfix infinite 1s;
                     animation: bugfix infinite 1s;
                 }
-                @-webkit-keyframes bugfix { 
-                    to { padding: 0; } 
+                @-webkit-keyframes bugfix {
+                    to { padding: 0; }
                 }
-        
                 @media (max-width: 350px) {
                     .chat-nav {
                         width: 100%;
                     }
-                }  
-                
+                }
                 .chat-message-text {
                     font-size: 18px;
                     line-height: 21px;
-                    
                     padding-left: 10px;
                     padding-top: 8px;
                     padding-right: 0px;
                     padding-bottom: 0px;
-                
                     color: #FFFFFF;
                 }
-        
                 .chat-message {
                     width: 343px;
-                    margin-top: 25px;
+                    margin-top: 5px;
                 }
-        
                 .your-chat-message{
                     float: right;
-                    right: 15px;
-                    
-        
-                    background: #377DFF;
-                    border-radius: 20px 20px 1px 20px;
                 }
-                .other-chat-message{
-                    float: left;
-                    left: 15px;
-        
-                    background: #FFFFFF;
-                    border-radius: 1px 20px 20px 20px;
-                }
-        
-        
                 .chat-message-text-other{
                     color: #363636;
+                    left: 15px;
+                    background: #FFFFFF;
+                    border-radius: 1px 20px 20px 20px;
+                    min-height: 45px;
                 }
-        
+                .chat-message-text-your{
+                    right: 15px;
+                    background: #377DFF;
+                    border-radius: 20px 20px 1px 20px;
+                    min-height: 45px;
+                    margin-bottom:5px;
+                }
                 .chat-message-user-type {
-                    position: absolute;
                     font-size: 14px;
                     line-height: 16px;
                 }
-        
                 .other-chat-message-type{
                     left: 15px;
                     color: #B4B4B4;
+                    margin-bottom:5px;
                 }
-        
                 .your-chat-message-type{
-                    right: 40px;
+                    right: 20px;
                     color: #B4B4B4;
                 }
-        
                 #chat-input{
                     position: absolute;
                     bottom: 0px;
@@ -187,11 +164,9 @@
                     border-radius: 20px;
                     padding: 5px;
                 }
-        
                 #chat-input:focus-visible {
-                    outline: -webkit-focus-ring-color auto 0px !important; 
+                    outline: -webkit-focus-ring-color auto 0px !important;
                 }
-        
                 #send-chat-message-icon{
                     position: absolute;
                     bottom: 4px;
@@ -202,6 +177,10 @@
                 #send-chat-message-icon:hover{
                     cursor: pointer;
                 }
+                .another-chat-message{
+                    float:left;
+                }
+
             `);
 
             $('#send-chat-message-icon').click(function () {
@@ -272,17 +251,17 @@
             if (messages != undefined) {
                 for (let i = 0; i < messages.length; i++) {
                     let messageHtml = `<div class="chat-message your-chat-message">
-                        <p class="chat-message-text">${messages[i]['text']}</p>
-                        <p class="chat-message-user-type your-chat-message-type">вы</p>
+                        <p class="chat-message-text chat-message-text-your">${messages[i]['text']}</p>
+                        <p class="chat-message-user-type your-chat-message-type your-chat-message">вы</p>
                     </div>`
 
                     if (messages[i]['user_info'] != userInfo) {
                         messageHtml = `
-                        <div class="chat-message other-chat-message">
-                            <p class="chat-message-text chat-message-text-other">${messages[i]['text']}</p>
-                            <p class="chat-message-user-type other-chat-message-type">${messages[i]['user']}</p>
-                        </div>
-                        `
+                            <div class="chat-message another-chat-message">
+                                <p class="chat-message-user-type other-chat-message-type">${messages[i]['user']}</p>
+                                <p class="chat-message-text chat-message-text-other">${messages[i]['text']}</p>
+                            </div>
+                            `
                     }
 
                     chatMessages.append(messageHtml);
